e6633c4dbaf28ff3fac64f0c646bc88cd858f227
toizy@yandex.ru <toizy>
2022-12-23 18:43:49 +0300

    Fix tabulation in the sources =\

--- includes/uAppSettings.pas
+++ includes/uAppSettings.pas
@@ -3,95 +3,96 @@
 interface
 
 uses
-    Vcl.Forms,
-    System.SysUtils,
-    System.IOUtils,
-    Winapi.Windows
+	Vcl.Forms,
+	System.SysUtils,
+	System.IOUtils,
+	Winapi.Windows
     ;
 
 type
-    TSettingsData = record
-    	Version: Integer;
-        DataDir: string;
-        ServersDir: string;
-    end;
+	TSettingsData = record
+		Version: Integer;
+		DataDir: string;
+		ServersDir: string;
+	end;
 
-    TSettings = record
-        Data: TSettingsData;
-    private
-        IsInitialized: Boolean;
-        SettingsFilename: string;
-        procedure InitWithDefaults;
-    public
-        procedure Init;
-        procedure Load;
-        procedure Save;
-    end;
+	TSettings = record
+		Data: TSettingsData;
+	private
+		IsInitialized: Boolean;
+		SettingsFilename: string;
+		procedure InitWithDefaults;
+	public
+		procedure Init;
+		procedure Load;
+		procedure Save;
+	end;
 
 var
-    Settings: TSettings;
+	Settings: TSettings;
 
 implementation
 
 uses
 	uJsonSerializer,
-    uCommonConsts
-    ;
+	uCommonConsts;
 
 const
-    FILE_VERSION = 0;
+	FILE_VERSION = 0;
 
-{ TSettings }
+	{ TSettings }
 
 procedure TSettings.Init;
 const
-    FILENAME = 'settings.json';
+	FILENAME = 'settings.json';
 begin
-    if (IsInitialized = False) then
+	if (IsInitialized = False) then
 		SettingsFilename := ExtractFilePath(Application.ExeName) + FILENAME;
-    IsInitialized := True;
+	IsInitialized := True;
 end;
 
 procedure TSettings.InitWithDefaults;
 begin
-    Data.Version := FILE_VERSION;
-    //Data.ServersDir := ExtractFilePath(TPath.GetHomePath) + 'Roaming\hl2mp_server_manager\servers';
-    //	../Data/
-    //      /Servers/
-    Data.DataDir := ExtractFilePath(Application.ExeName) + 'Data\';
+	Data.Version := FILE_VERSION;
+	// Data.ServersDir := ExtractFilePath(TPath.GetHomePath) + 'Roaming\hl2mp_server_manager\servers';
+	// ../Data/
+	// /Servers/
+	Data.DataDir := ExtractFilePath(Application.ExeName) + 'Data\';
 	Data.ServersDir := 'Servers';
-    TDirectory.CreateDirectory(Data.DataDir);
-    TDirectory.CreateDirectory(Data.DataDir + Data.ServersDir);
-    Save;
+	TDirectory.CreateDirectory(Data.DataDir);
+	TDirectory.CreateDirectory(Data.DataDir + Data.ServersDir);
+	Save;
 end;
 
 procedure TSettings.Load;
 var
-    Content: string;
+	Content: string;
 begin
-    //if not FileExists(SettingsFilename) then
-    	InitWithDefaults;
+	// if not FileExists(SettingsFilename) then
+	InitWithDefaults;
 
-    Content := TFile.ReadAllText(SettingsFilename, TEncoding.UTF8);
+	Content := TFile.ReadAllText(SettingsFilename, TEncoding.UTF8);
 	Data := DSON.fromJson<TSettingsData>(Content);
 
-    if (Data.Version <> FILE_VERSION) then
-    begin
-    	var Msg := PChar(Format(T_SETTINGS_VERSION_CONFLICT, [FILE_VERSION, Data.Version]));
-    	Application.MessageBox(Msg, T_WARNING, MB_OK);
-    end;
+	if (Data.Version <> FILE_VERSION) then
+	begin
+		var
+		Msg := PChar(Format(T_SETTINGS_VERSION_CONFLICT, [FILE_VERSION, Data.Version]));
+		Application.MessageBox(Msg, T_WARNING, MB_OK);
+	end;
 end;
 
 procedure TSettings.Save;
 var
-    Content: string;
+	Content: string;
 begin
-    Content := DSON.toJson<TSettingsData>(Data, True);
-    TFile.WriteAllText(SettingsFilename, Content, TEncoding.UTF8);
+	Content := DSON.toJson<TSettingsData>(Data, True);
+	TFile.WriteAllText(SettingsFilename, Content, TEncoding.UTF8);
 end;
 
 initialization
-    Settings.Init;
-    Settings.Load;
+
+Settings.Init;
+Settings.Load;
 
 end.
--- includes/uServerSettings.pas
+++ includes/uServerSettings.pas
@@ -3,80 +3,80 @@ unit uServerSettings;
 interface
 
 uses
-    Vcl.Forms,
-    System.SysUtils,
-    System.IOUtils,
-    Winapi.Windows,
-    uArrayHelper
+	Vcl.Forms,
+	System.SysUtils,
+	System.IOUtils,
+	Winapi.Windows,
+	uArrayHelper
     ;
 
 type
-    TServerCommands = record
-    	Commands: TArrayRecord<TServerCommands>;
-        Name: string;
-        Cmd: string;
-        Arg: string;
-        NeedConfirmation: Boolean;
+	TServerCommands = record
+		Commands: TArrayRecord<TServerCommands>;
+		Name: string;
+		Cmd: string;
+		Arg: string;
+		NeedConfirmation: Boolean;
 	end;
 
-    TSettingsData = record
-    	Version: Integer;
-        Name: string;
-        Host: string;
-        Port: string;
-        Password: string;
-        Description: string;
-        Commands: TServerCommands;
-    end;
+	TSettingsData = record
+		Version: Integer;
+		Name: string;
+		Host: string;
+		Port: string;
+		Password: string;
+		Description: string;
+		Commands: TServerCommands;
+	end;
 
-    TServerSettings = record
-        Data: TSettingsData;
-    private
-        SettingsFilename: string;
-    public
-        procedure Load(Filename: string = '');
-        procedure Save(Filename: string = '');
-    end;
+	TServerSettings = record
+		Data: TSettingsData;
+	private
+		SettingsFilename: string;
+	public
+		procedure Load(Filename: string = '');
+		procedure Save(Filename: string = '');
+	end;
 
 var
-    ServerSettings: TServerSettings;
+	ServerSettings: TServerSettings;
 
 implementation
 
 uses
 	uJsonSerializer,
-    uCommonConsts
-    ;
+	uCommonConsts;
 
 const
-    FILE_VERSION = 0;
+	FILE_VERSION = 0;
 
-{ TSettings }
+	{ TSettings }
 
 procedure TServerSettings.Load(Filename: string = '');
 var
-    Content: string;
+	Content: string;
 begin
 	if (Filename <> '') then
-    	SettingsFilename := Filename;
-    Content := TFile.ReadAllText(SettingsFilename, TEncoding.UTF8);
+		SettingsFilename := Filename;
+	Content := TFile.ReadAllText(SettingsFilename, TEncoding.UTF8);
 	Data := DSON.fromJson<TSettingsData>(Content);
-    if (Data.Version <> FILE_VERSION) then
-    begin
-    	var Msg := PChar(Format(T_SETTINGS_VERSION_CONFLICT, [FILE_VERSION, Data.Version]));
-    	Application.MessageBox(Msg, T_WARNING);
-        Application.Terminate;
-    end;
+	if (Data.Version <> FILE_VERSION) then
+	begin
+		var
+		Msg := PChar(Format(T_SETTINGS_VERSION_CONFLICT, [FILE_VERSION, Data.Version]));
+		Application.MessageBox(Msg, T_WARNING);
+		Application.Terminate;
+	end;
 end;
 
 procedure TServerSettings.Save(Filename: string = '');
 var
-    Content: string;
+	Content: string;
 begin
-    if (Filename <> '') then
-    	SettingsFilename := Filename;
-    Content := DSON.toJson<TSettingsData>(Data, True);
-    TFile.WriteAllText(SettingsFilename, Content, TEncoding.UTF8);
+	if (Filename <> '') then
+		SettingsFilename := Filename;
+	Content := DSON.toJson<TSettingsData>(Data, True);
+	TFile.WriteAllText(SettingsFilename, Content, TEncoding.UTF8);
 end;
 
 end.
